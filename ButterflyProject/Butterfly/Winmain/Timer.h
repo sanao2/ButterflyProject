5c35e6fd7647b211e60304646ca3c289f614bcfe
sanao2 <SanaoAiza1102@gmail.com>
2025-04-23 17:19:46 +0900

    Add files via upload

---
+++ ButterflyProject/Butterfly/Winmain/Timer.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include <time.h>
+#include <windows.h>
+#include <stdio.h>
+
+
+class Timer
+{
+private : 
+	clock_t startclock;		// startTime 
+	clock_t endclock;		// endTime 
+	double  deltatime;		// deltaTime 
+	double  cur_Time = 0;    // currentTime 
+
+public:
+	void TimeReset() { deltatime = 0; };
+	void TimeShoutdown(); // Time ShotDown
+	void TimeRender();	  // Time Render 
+
+	// Timer Get code 
+	clock_t GetStartTime() { return startclock;  }
+	clock_t GetEndTime() { return startclock;  }
+
+	Timer(double time);   // Timer Create 
+	~Timer();			  // Timer Delete  
+};
